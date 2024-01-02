aa360dd339f80757f246edb297fdb6434281c9e8
itx-shoaib <shoaibjamil43@gmail.com>
2024-01-02 11:58:47 +0500

    Attaching mongodb with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'), // Loaded from .ENV
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  // constructor(private configService: ConfigService) {}
   getHello(): string {
+    // const db = this.configService.get<string>('MONGO_URI')
     return 'Hello World!';
   }
 }
